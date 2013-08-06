6931663006059e1f51f8d05b8f00b9e43dce7119
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-06 13:24:51 -0700

    Moving some of the annotation names to statics strings and referencing them directly instead of copying them all over the place.

---
+++ telemetry-lib/src/main/java/com/yammer/telemetry/tracing/AnnotationNames.java
@@ -0,0 +1,7 @@
+package com.yammer.telemetry.tracing;
+
+public class AnnotationNames {
+    public static final String SERVER_SENT = "ServerSent";
+    public static final String SERVER_RECEIVED = "ServerReceived";
+    public static final String SERVICE_NAME = "ServiceName";
+}
