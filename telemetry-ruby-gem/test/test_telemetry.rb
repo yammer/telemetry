bcf049beb27375d04165d4a2ad4640589985d672
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-09 16:14:57 -0700

    Add a unit test to verify span attaching works.

--- telemetry-ruby-gem/test/test_telemetry.rb
+++ telemetry-ruby-gem/test/test_telemetry.rb
@@ -57,4 +57,23 @@ class SpanTest < Test::Unit::TestCase
     trace = @memory_sink.trace(span.trace_id)
     assert_equal([['ServiceStart', nil], ['ServiceName', 'unittests']], trace.annotations(span).map { |s| [s.name, s.message]})
   end
+
+  def test_attach_span
+    trace_id = UUIDTools::UUID.random_create
+    span_id = UUIDTools::UUID.random_create
+    span = Telemetry::Span.attach_span(trace_id, span_id)
+    span.add_annotation('Attached', 'Yay')
+
+    span2 = Telemetry::Span.start_trace('subspan')
+    span2.add_annotation('SubSpan', 'w00t')
+    span2.end
+
+    span.end
+
+    trace = @memory_sink.trace(span.trace_id)
+
+    assert_equal([['Attached', 'Yay']], trace.annotations(span).map { |s| [s.name, s.message]})
+    assert_equal([span2], trace.children(span))
+    assert_equal([['SubSpan', 'w00t']], trace.annotations(span2).map { |s| [s.name, s.message]})
+  end
 end
