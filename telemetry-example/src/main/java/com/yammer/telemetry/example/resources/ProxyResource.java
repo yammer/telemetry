9b0636d469187e855c7db59ba8305af3485a120a
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-30 14:41:24 -0700

    Preserve the content type of the proxied content.

--- telemetry-example/src/main/java/com/yammer/telemetry/example/resources/ProxyResource.java
+++ telemetry-example/src/main/java/com/yammer/telemetry/example/resources/ProxyResource.java
@@ -1,17 +1,16 @@
 package com.yammer.telemetry.example.resources;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 @Path("/proxy/to/{hostname}/{uri:.*}")
-@Produces(MediaType.TEXT_PLAIN)
 public class ProxyResource {
     private final Client client;
 
@@ -20,7 +19,10 @@ public class ProxyResource {
     }
 
     @GET
-    public String proxyTo(@PathParam("hostname") String hostname, @PathParam("uri") String uri, @Context UriInfo uriInfo) {
-        return client.resource("http://" + hostname + "/" + uri).queryParams(uriInfo.getQueryParameters()).get(String.class);
+    public Response proxyTo(@PathParam("hostname") String hostname, @PathParam("uri") String uri, @Context UriInfo uriInfo) {
+        final ClientResponse response = client.resource("http://" + hostname + "/" + uri)
+                                              .queryParams(uriInfo.getQueryParameters())
+                                              .get(ClientResponse.class);
+        return Response.ok(response.getEntityInputStream()).type(response.getType()).build();
     }
 }
