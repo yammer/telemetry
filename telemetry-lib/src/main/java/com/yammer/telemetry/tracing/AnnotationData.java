300e5c858393f0c6869fb8ec7985537774edb0a2
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-30 17:03:39 -0700

    Remove IntelliJ generated header comment.

--- telemetry-lib/src/main/java/com/yammer/telemetry/tracing/AnnotationData.java
+++ telemetry-lib/src/main/java/com/yammer/telemetry/tracing/AnnotationData.java
@@ -1,12 +1,5 @@
 package com.yammer.telemetry.tracing;
 
-/**
- * Created with IntelliJ IDEA.
- * User: rkennedy
- * Date: 8/13/13
- * Time: 10:02 PM
- * To change this template use File | Settings | File Templates.
- */
 public interface AnnotationData {
     long getStartTimeNanos();
 
